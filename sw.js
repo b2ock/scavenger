2edd169afe88f30f3ac4b5082c4f9dd9b5cf827c
b2ock <118326205+b2ock@users.noreply.github.com>
2025-09-05 17:34:27 -0600

    turned into a pwa

---
+++ sw.js
@@ -0,0 +1,39 @@
+const cacheName = 'take-picture-app-v1';
+const assetsToCache = [
+  './',
+  './index.html',
+  './styles.css',
+  './script.js',
+  './manifest.json',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
+];
+
+// Install event: cache all assets
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(cacheName).then((cache) => {
+      return cache.addAll(assetsToCache);
+    })
+  );
+});
+
+// Activate event: cleanup old caches
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.map((key) => {
+        if (key !== cacheName) return caches.delete(key);
+      }))
+    )
+  );
+});
+
+// Fetch event: serve cached assets first
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((res) => {
+      return res || fetch(event.request);
+    })
+  );
+});
